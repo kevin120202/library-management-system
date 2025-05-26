cabd5ebefa50f3984cca2521ffac9944b06ab0c3
Kevin Dela Paz <kevindelapaz21@gmail.com>
2025-05-26 12:31:54 -0500

    created tokens table for auth

---
+++ internal/tokens/tokens.go
@@ -0,0 +1,11 @@
+package tokens
+
+import "time"
+
+type Token struct {
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserID    int       `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
+}
